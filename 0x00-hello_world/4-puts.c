22af6efe0f8a612240a20ae3812ca4ac867071fc
Kgethontle <itumelengpelonomi6@gmail.com>
2022-06-06 03:34:53 -0700

    Write a script that generates the assembly code (Intel syntax) of a C code and save it in an output file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+#include <stdio.h>                                                                       
+
+/**                                                                                      
+ *
+ * *main - prints a string that in pups the functions                                       
+ *
+ * *Description: Printing "Programming is like building a multilingual puzzle"              
+ *
+ * *Return: Always 0 (Success)                                                              
+ *
+ * */                                                                                       
+
+int main(void)                                                                           
+
+{                                                                                        
+
+	puts("\"Programming is like building a multilingual puzzle");                            
+
+	return (0);
